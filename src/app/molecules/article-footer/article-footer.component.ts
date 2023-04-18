ff404570163936a41134399befe3ca964e08a195
Philipp Doerner <philippmdoerner@web.de>
2023-04-18 20:17:21 +0200

    Add article footer component

---
+++ src/app/molecules/article-footer/article-footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-article-footer',
+  templateUrl: './article-footer.component.html',
+  styleUrls: ['./article-footer.component.scss']
+})
+export class ArticleFooterComponent {
+  @Input() link!: string;
+  @Input() label!: string;
+  @Input() deleteMessage: string = 'Delete this entry?';
+  @Input() showDelete: boolean = true;
+  @Output() buttonClick: EventEmitter<null> = new EventEmitter();
+  @Output() delete: EventEmitter<null> = new EventEmitter();
+
+}
--- src/app/molecules/molecules.module.ts
+++ src/app/molecules/molecules.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AtomsModule } from '../atoms/atoms.module';
+import { ArticleFooterComponent } from './article-footer/article-footer.component';
+import { ConfirmationToggleButtonComponent } from './confirmation-toggle-button/confirmation-toggle-button.component';
 import { EditToggleComponent } from './edit-toggle/edit-toggle.component';
 import { IconToggleButtonComponent } from './toggle-button/icon-toggle-button.component';
-import { ConfirmationToggleButtonComponent } from './confirmation-toggle-button/confirmation-toggle-button.component';
 
 
 
@@ -12,10 +14,12 @@ import { ConfirmationToggleButtonComponent } from './confirmation-toggle-button/
     EditToggleComponent,
     IconToggleButtonComponent,
     ConfirmationToggleButtonComponent,
+    ArticleFooterComponent,
   ],
   imports: [
     CommonModule,
     AtomsModule,
+    RouterModule,
   ]
 })
 export class MoleculesModule { }

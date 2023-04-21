a08027f35660e19065cfe956d6a8168ce9cb9cdf
Philipp Doerner <philippmdoerner@web.de>
2023-04-21 22:04:07 +0200

    Mini example update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nimstoryfont-gui';
+  form = new FormGroup({});
+  model = {};
+  options = {};
+  fields = [
+    {
+      key: 'text',
+      type: 'text-editor',
+    },
+  ];
+  
+  constructor(){console.log(this)};
 }

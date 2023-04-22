709e603ad2a06ab5292f4d0aeee5c580c94fca88
Philipp Doerner <philippmdoerner@web.de>
2023-04-22 18:42:34 +0200

    implement formly service

---
+++ src/app/_services/formly/formly-service.service.ts
@@ -0,0 +1,337 @@
+import { Injectable } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { Observable, map, of } from 'rxjs';
+import { FormlyCheckboxConfig, FormlyDatepickerConfig, FormlyFileConfig, FormlyInputConfig, FormlyInterface, FormlyOverviewDisabledSelectConfig, FormlyOverviewSelectConfig, FormlyPasswordInterface, FormlyCustomSelectConfig as FormlyStaticSelectConfig, FormlyCustomStringSelectConfig as FormlyStaticStringSelectConfig, StaticOption } from 'src/app/_models/formly';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormlyServiceService {
+
+  constructor(
+    // private overviewService: any,
+  ) { }
+  
+  buildOverviewSelectConfig(config: FormlyOverviewSelectConfig): FormlyFieldConfig{
+    const options$: Observable<any[]> = this.getOverviewItems(config);
+    const validators = this.getValidators(config);
+
+    return {
+      key: config.key,
+      type: 'select',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        labelProp: config.labelProp,
+        valueProp: config.valueProp ?? 'pk',
+        options: options$,
+        required: config.required ?? true,
+        disabled: config.disabled,
+      },
+      validators: {
+        validation: validators
+      }
+    };
+  }
+  
+  buildDisableSelectConfig(config: FormlyOverviewDisabledSelectConfig): FormlyFieldConfig{
+    const options$: Observable<any[]> = this.getOverviewItems(config);
+    const validators = this.getValidators(config);
+    
+    return {
+      key: config.key,
+      type: 'select-disable',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide ?? false,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        labelProp:  config.labelProp,
+        valueProp:  config.valueProp ?? 'pk',
+        options: options$,
+        required: config.required ?? true,
+        disabledExpression: config.disabledExpression,
+        tooltipMessage: config.tooltipMessage,
+        warningMessage: config.warningMessage,
+        additionalProperties: {
+          showWrapperLabel: config.showWrapperLabel ?? true,
+        }
+      },
+      validators: {
+        validation: validators
+      },
+    };
+  }
+  
+  buildStaticSelectConfig(config: FormlyStaticSelectConfig): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+
+    return {
+      key: config.key,
+      type: 'select',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide ?? false,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        options: config.options,
+        required: config.required ?? true,
+        disabled: config.disabled,
+      },
+      validators: {
+        validation: validators,
+      },
+    }
+  }
+  
+  buildStaticStringSelectConfig(partialConfig: FormlyStaticStringSelectConfig): FormlyFieldConfig{
+    const optionStrings: string[] = partialConfig.options;
+    
+    let options: StaticOption[] = optionStrings.map(str => {
+      return {label: str, value: str}
+    });
+    
+    const config: FormlyStaticSelectConfig = {
+      ...partialConfig,
+      options
+    };
+    
+    return this.buildStaticSelectConfig(config);
+  }
+    
+  buildInputConfig(config: FormlyInputConfig): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+    if(config.inputKind === 'NUMBER'){
+      validators.push('notInteger');
+    }
+    if(config.inputKind === 'NAME'){
+      //Why 'hasSpecialCharacters' validation? Names are used in URLs, they mustn't have special characters
+      validators.push('hasSpecialCharacters');
+    }
+    
+    let innerInputType: 'string' | 'number';
+    switch(config.inputKind){
+      case 'NUMBER':
+        innerInputType = 'number';
+        break;
+      default:
+        innerInputType = 'string'
+    }
+    
+    return {
+      key: config.key,
+      type: 'input',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide ?? false,
+      parsers: config.parsers,
+      props:{
+        maxLength: config.maxLength,
+        minLength: config.minLength,
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        type: innerInputType,
+        required: config.required ?? true,
+        disabled: !!config.disabled,
+        placeholder: config.placeholder ?? undefined,
+      },
+      validators:{
+        validation: validators,
+      }
+    }
+  }
+  
+  buildSinglePasswordConfig(config: FormlyInterface): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+
+    return {
+      key: config.key,
+      type: 'input',
+      className: config.className,
+      fieldGroupClassName: config.fieldGroupClassName,
+      templateOptions:{
+        label: config.label ?? 'Password',
+        type: 'password',
+        required: true,
+        placeholder: 'Your password',
+        disabled: config.disabled,
+      },
+      validators:{
+        validation: validators
+      }
+    }
+  }
+  
+  buildConfirmedPasswordConfig(config: FormlyPasswordInterface): FormlyFieldConfig{
+    const validators = config.validators ?? [];
+    validators.push('required');
+    
+    const passwordField = {
+      key: 'password', //Hard coded, fieldMatch validator depends on this
+      type: 'input',
+      className: config.className,
+      templateOptions:{
+        label: config.label ?? 'Password',
+        type: 'password',
+        required: true,
+        placeholder: 'Password, at least 7 characters',
+        disabled: config.disabled,
+      },
+      validators:{
+        validation: validators
+      }
+    }
+
+    const confirmPasswordField = {
+      key: 'passwordConfirm', //Hard coded, fieldMatch validator depends on this
+      type: 'input',
+      className: config.className,
+      templateOptions:{
+        label: (config.label) ? 'Confirm ' + config.label : 'Password Confirmation',
+        type: 'password',
+        required: true,
+        placeholder: 'Please re-enter your password',
+        disabled: config.disabled,
+      },
+    }
+
+    return {
+      validators: {
+        validation: [
+          { name: 'fieldMatch', options: { errorPath: 'passwordConfirm' } },
+        ]
+      },
+      fieldGroup: [
+        passwordField,
+        confirmPasswordField
+      ]
+    }
+  }
+  
+  buildCheckboxConfig(config: FormlyCheckboxConfig): FormlyFieldConfig{
+    return{
+      key: config.key,
+      type: 'checkbox',
+      className: config.className,
+      wrappers: config.wrappers,
+      defaultValue: config.defaultValue,
+      hideExpression: config.hide,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        required: config.required ?? true,
+        disabled: config.disabled,
+      },
+    }
+  }
+  
+  buildDatepickerConfig(config: FormlyDatepickerConfig): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+    validators.push('date');
+
+    return {
+      key: config.key,
+      type: 'datepicker',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        required: config.required ?? true,
+        disabled: config.disabled,
+      },
+      validators:{
+        validation: validators,
+      }
+    }
+  }
+  
+  buildFileFieldConfig(config: FormlyFileConfig): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+    
+    return {
+      key: config.key,
+      type: 'file',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide,
+      props: {
+        buttonType: config.fileButtonType ?? 'SECONDARY',
+        fileFieldKind: config.fileFieldKind ?? 'IMAGE',
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        required: config.required ?? true,
+        disabled: !!config.disabled,
+      },
+      validators:{
+        validation: validators,
+      }
+    }
+  }
+  
+  genericTextField(config: FormlyInterface): FormlyFieldConfig{
+    const validators = this.getValidators(config);
+    
+    return {
+      key: config.key,
+      type: 'text-editor',
+      className: config.className,
+      wrappers: config.wrappers,
+      hideExpression: config.hide,
+      props:{
+        label: config.label ?? this.capitalizeFirstLetter(config.key),
+        required: config.required ?? true,
+        disabled: config.disabled,
+      },
+      validators:{
+        validation: validators
+      }
+    }
+  }
+
+  
+  private getValidators(config: FormlyInterface){
+    const validators = config.validators ?? [];
+    if (config.required) {
+      validators.push('required');
+    }
+    
+    return validators;
+  }
+  
+  private getOverviewItems(config: FormlyOverviewSelectConfig): Observable<any[]> {
+    // const isCampaignSpecific = config.campaign !== null;
+    // isCampaignSpecific 
+    //   ? this.selectOptionService.getCampaignOverviewItems(config.campaign, config.overviewType, config.sortProp)
+    //   : this.selectOptionService.getAllOverviewItems(config.overviewType, config.sortProp);
+    
+    let options: Observable<any[]> = of([]);
+    
+    if(!config.required){
+      options = this.addEmptyOption(options, config);
+    }
+    
+    return options;
+  }
+  
+  private addEmptyOption(observable: Observable<any[]>, config: FormlyOverviewSelectConfig): Observable<any[]>{
+    return observable.pipe(
+      map(values => [
+        this.createEmptyOption(config), 
+        ...values
+      ]),
+    );
+  }
+  
+  private createEmptyOption(config: FormlyOverviewSelectConfig){
+    const emptyOption: any = {};
+    emptyOption[config.key] = '------';
+    const valueProp: string = config.valueProp ?? config.key;
+    emptyOption[valueProp] = null;
+    return emptyOption;
+  }
+  
+  
+  private capitalizeFirstLetter(s: string){
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  }
+}

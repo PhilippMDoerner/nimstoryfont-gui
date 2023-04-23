afdc47d09ea76101cb4d3b538d367bcfac77b39b
Philipp Doerner <philippmdoerner@web.de>
2023-04-23 16:40:01 +0200

    Grant interactive badge to have custom styling for icon-parts

--- src/app/atoms/interactive-badge/interactive-badge.component.ts
+++ src/app/atoms/interactive-badge/interactive-badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ElementType } from '../_models/button';
 import { Icon } from '../_models/icon';
 
@@ -7,12 +7,17 @@ import { Icon } from '../_models/icon';
   templateUrl: './interactive-badge.component.html',
   styleUrls: ['./interactive-badge.component.scss']
 })
-export class InteractiveBadgeComponent {
+export class InteractiveBadgeComponent implements OnInit{
   @Input() type!: ElementType;
   @Input() text!: string;
   @Input() textLink?: string;
   @Input() icon?: Icon;
+  @Input() iconType?: ElementType;
   
   @Output() iconClick: EventEmitter<null> = new EventEmitter();
   @Output() labelClick: EventEmitter<null> = new EventEmitter();
+  
+  ngOnInit(): void {
+    this.iconType = this.iconType ?? this.type;
+  }
 }

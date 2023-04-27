03a644807ccda04f29d1c05d4558a448122a9e34
Philipp Doerner <philippmdoerner@web.de>
2023-04-27 23:29:44 +0200

    Replicate discord sidebar behaviour

--- src/app/organisms/page/page.component.ts
+++ src/app/organisms/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { Campaign } from 'src/app/_models/campaign';
 import { UserData } from 'src/app/_models/token';
 
@@ -11,4 +11,71 @@ export class PageComponent {
   @Input() user!: UserData;
   @Input() campaign!: Campaign;
   @Input() serverUrl!: string;
+  
+  @ViewChild('sidebar') sidebarElement!: ElementRef;
+  @ViewChild('content') contentElement!: ElementRef;
+  
+  startX: number = 0;
+  currentX: number = 0;
+  threshold: number = 50;
+  sidebarWidth: number = 20*16;
+  showSidebar: boolean = false;
+  
+  mobileWidth: number = 576;
+  
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    if(!this.isMobile()){
+      return;
+    }
+    
+    this.startX = event.touches[0].clientX;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    if(!this.isMobile()){
+      return;
+    }
+    
+    const currentX = event.touches[0].clientX;
+    const distanceX = currentX - this.startX;
+
+    if (distanceX > 0 && distanceX <= this.sidebarWidth) {
+      this.slideRightViaMargin(distanceX);
+    }
+  }
+  
+  slideRightViaMargin(newMargin: number){    
+    const element: HTMLElement = this.contentElement.nativeElement;
+    element.style.marginLeft = `${newMargin}px`;
+  }
+
+  resetSlide(){
+    const element: HTMLElement = this.contentElement.nativeElement;
+    element.style.marginLeft = '';
+  }
+  
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    if(!this.isMobile()){
+      return;
+    }
+    
+    const currentX = event.changedTouches[0].clientX;
+    const distanceX = currentX - this.startX;
+
+    if (distanceX > this.threshold) {
+      this.showSidebar = true;
+    } else if (distanceX <= this.threshold) {
+      this.showSidebar = false;
+    }
+
+    this.resetSlide();
+    this.startX = 0;
+  }
+  
+  isMobile(){
+    return screen.width <= this.mobileWidth;
+  }
 }

5fdbe2de54f0f4eda8bf5e67c6467b945300ca28
Philipp Doerner <philippmdoerner@web.de>
2023-04-29 13:03:09 +0200

    Add groupBy pipe

--- src/app/atoms/_pipes/groupObjects.ts
+++ src/app/atoms/_pipes/groupObjects.ts
@@ -17,3 +17,53 @@ export class GroupByFirstLetterPipe implements PipeTransform{
         
     }
 }
+
+
+@Pipe({name: "groupBy"}) 
+export class GroupByPipe implements PipeTransform{
+    transform(itemArray: Array<any>, groupProp: string): any{
+        const callback = (accumulator: any, item: any) => {
+            //grouped Field Value = The content of the field by which you're grouping.
+            //item = The value being grouped
+            //Accumulator = Object with groupedFieldValue as key and an array of items associated with that Field as value
+            const groupedFieldValue =  this.getFieldValue(groupProp, item);
+            
+            const hasGroupAlready = accumulator.hasOwnProperty(groupedFieldValue);
+            if (hasGroupAlready){
+                accumulator[groupedFieldValue].push(item);
+            } else {
+                accumulator[groupedFieldValue] = [item];
+            }
+
+            return accumulator;
+        }
+        
+        const groupedObj: {[key: string]: any[]} = itemArray.reduce(callback, {});
+        const groupArray: {key: string, value: any[]}[] = Object.keys(groupedObj)
+            .map(key => ({ 
+                key, 
+                value: groupedObj[key] 
+            }));
+        const sortedGroupArray: {key: string, value: any[]}[] = groupArray.sort(
+            (group1, group2) => group1.key.toLowerCase() < group2.key.toLowerCase() ? 1 : -1
+        );
+
+        return sortedGroupArray.reverse();
+    }
+    
+    /**
+     * Essentially evaluates an expression "item.field1.field2.field3" with fieldPath being "field1.field2.field3".
+     * It iterates over the individual fields and goes through them one by one, returns the last field value.
+     */
+    getFieldValue(fieldPath: string, item: object): any{
+        /**Returns the field of a given item, even if it is nested within it */
+        const keys: string[] = fieldPath.split(".");
+
+        let currentValue: any = item;
+        for (let key of keys){
+            currentValue = currentValue[key];
+        }
+
+        return currentValue;
+    }
+}

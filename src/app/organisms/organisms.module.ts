930a35582a4ade2ce185d821122a123fb4c55019
Philipp Doerner <philippmdoerner@web.de>
2023-04-30 16:23:19 +0200

    Add image grid

--- src/app/organisms/organisms.module.ts
+++ src/app/organisms/organisms.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormlyModule } from '@ngx-formly/core';
 import { TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 import { MoleculesModule } from '../molecules/molecules.module';
+import { FilterListComponent } from './filter-list/filter-list.component';
 import { FormlyDatepickerFieldComponent } from './formly-datepicker-field/formly-datepicker-field.component';
 import { FormlyEditorFieldComponent } from './formly-editor-field/formly-editor-field.component';
 import { FormlySelectDisableFieldComponent } from './formly-select-disable/formly-select-disable-field.component';
+import { IconCardListComponent } from './icon-card-list/icon-card-list.component';
+import { ImageGridComponent } from './image-grid/image-grid.component';
 import { PageContainerComponent } from './page-container/page-container.component';
 import { PageComponent } from './page/page.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { FilterListComponent } from './filter-list/filter-list.component';
-import { IconCardListComponent } from './icon-card-list/icon-card-list.component';
 
 
 
@@ -24,6 +25,7 @@ import { IconCardListComponent } from './icon-card-list/icon-card-list.component
     PageComponent,
     FilterListComponent,
     IconCardListComponent,
+    ImageGridComponent,
   ],
   imports: [
     CommonModule,
@@ -38,6 +40,9 @@ import { IconCardListComponent } from './icon-card-list/icon-card-list.component
     MoleculesModule,
     SidebarComponent,
     PageComponent,
+    FilterListComponent,
+    IconCardListComponent,
+    ImageGridComponent,
   ],
   providers: [
     { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce.min.js' },

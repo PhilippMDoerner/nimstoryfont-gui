3738a2cd988c80c6fe54bf0a8a67943eb0da4454
Philipp Doerner <philippmdoerner@web.de>
2023-05-02 07:46:37 +0200

    Add some missing exports to index files

--- src/app/molecules/index.ts
+++ src/app/molecules/index.ts
@@ -4,6 +4,7 @@ export { DEFAULT_SEARCH_PREFERENCES, SidebarOption } from "./_models/search-pref
 export { ToggleState } from "./_models/toggle";
 export { ArticleFooterComponent } from './article-footer/article-footer.component';
 export { BadgeListComponent } from './badge-list/badge-list.component';
+export { ChoiceSelectComponent } from './choice-select/choice-select.component';
 export { CollapsiblePanelComponent } from './collapsible-panel/collapsible-panel.component';
 export { CompareFormComponent } from './compare-form/compare-form.component';
 export { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
@@ -11,8 +12,11 @@ export { ConfirmationToggleButtonComponent } from './confirmation-toggle-button/
 export { EditToggleComponent } from './edit-toggle/edit-toggle.component';
 export { FormComponent } from './form/form.component';
 export { FormlyFileFieldComponent } from './formly-file-field/formly-file-field.component';
+export { IconCardComponent } from './icon-card/icon-card.component';
+export { ImageCardComponent } from './image-card/image-card.component';
 export { ListComponent } from './list/list.component';
 export { PageBackgroundComponent } from './page-background/page-background.component';
+export { SearchFieldComponent } from './search-field/search-field.component';
 export { SidebarLegendComponent } from './sidebar-legend/sidebar-legend.component';
 export { SmallCreateFormComponent } from './small-create-form/small-create-form.component';
 

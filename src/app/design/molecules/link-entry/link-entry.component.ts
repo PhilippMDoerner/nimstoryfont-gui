1cc2afa1a32259f70451a0ef9ad88c09e3b94d61
Philipp Doerner <philippmdoerner@web.de>
2023-05-06 12:17:10 +0200

    Improve styling

--- src/app/design/molecules/link-entry/link-entry.component.ts
+++ src/app/design/molecules/link-entry/link-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LinkEntry } from '../_models/link-entry';
 
 type State = "DISPLAY" | "DELETE";
@@ -8,16 +8,20 @@ type State = "DISPLAY" | "DELETE";
   templateUrl: './link-entry.component.html',
   styleUrls: ['./link-entry.component.scss']
 })
-export class LinkEntryComponent<T>{
+export class LinkEntryComponent<T> implements OnInit{
   @Input() entry!: LinkEntry<T>;
   @Input() canDelete: boolean = false;
-  @Input() columnSizes: number[] = this.canDelete ? [3, 8, 1] : [3, 9, 0];
+  @Input() columnSizes!: number[];
   @Input() deleteMessage: string = 'Delete entry?';
   
   @Output() delete: EventEmitter<any> = new EventEmitter();
   @Output() linkClick: EventEmitter<any> = new EventEmitter();
   
   state: State = 'DISPLAY';
+  
+  ngOnInit(): void {
+    this.columnSizes = this.canDelete ? [3, 8, 1] : [3, 9, 0]
+  }
   
   changeState(newState: State){
     this.state = newState;

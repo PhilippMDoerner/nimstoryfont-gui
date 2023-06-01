a75c5069eb6e944bd79001dd421f152bb2907118
Philipp Doerner <philippmdoerner@web.de>
2023-06-01 20:11:01 +0200

    Add mock services to contain mock-data

--- src/app/design/templates/map/map.stories.ts
+++ src/app/design/templates/map/map.stories.ts
@@ -4,75 +4,11 @@ import { action } from '@storybook/addon-actions';
 import { Meta, StoryFn, moduleMetadata } from '@storybook/angular';
 import * as x from 'leaflet';
 import { OverviewItem } from 'src/app/_models/overview';
+import { dummyMarkers } from 'src/app/_services/article/marker.service.mock';
 import { ExtendedMap } from '../../../_models/map';
-import { MapMarker } from '../../../_models/mapMarker';
 import { OrganismsModule } from '../../organisms';
 import { MapComponent } from './map.component';
 const y = x;
-
-
-const dummyMarkers: MapMarker[] = [
-  {
-    getAbsoluteRouterUrl: () => "/marker1/url",
-    pk: 106,
-    color: undefined,
-    icon: undefined,
-    longitude: 532,
-    latitude: 553,
-    map: 2,
-    map_details: {
-      name: "The World"
-    },
-    location: 50,
-    location_details: {
-      name: "Galway",
-      description: "<p>The New Capital of Fen's High Kingdom</p>\n<p>&nbsp;</p>\n<p>Galway City Breakdown</p>\n<p>Eastern Lakeway - 15 Buildings<br />Western Lakeway - 40 Buildings<br />Myria Island - 7 Buildings<br />River District - 46 Buildings, several temporary Refugee Shelters<br />Forest District - 85 Buildings<br />Hill District - 149 Buildings<br />Galway Proper - 60 Buildings</p>\n<p>Total - 402 Civilian Buildings in Galway</p>\n<p>Estimated Population: 4000+</p>",
-      parent_location_name: "none",
-      sublocations: []
-    },
-    type: 11,
-    type_details: {
-      name: "Settlement",
-      icon: "home",
-      is_text_marker: false,
-      fontawesome_type: "fa",
-      color: "lightgreen",
-      pk: 11
-    },
-    campaign_details: { name: 'Aldrune', id: 1},
-  },
-  {
-    getAbsoluteRouterUrl: () => "/marker2/url",
-    pk: 137,
-    color: undefined,
-    icon: undefined,
-    longitude: 752,
-    latitude: 579,
-    map: 2,
-    map_details: {
-      name: "The World"
-    },
-    location: 206,
-    location_details: {
-      name: "Eastern sea",
-      description: "<p>The ocean east of Aldrune, now accessible through the path carved by the world serpent.</p>",
-      parent_location_name: "none",
-      sublocations: [
-        "Lighthouse"
-      ]
-    },
-    type: 23,
-    type_details: {
-      name: "Sea/Ocean",
-      icon: "water",
-      is_text_marker: true,
-      fontawesome_type: "fa",
-      color: "gray",
-      pk: 23
-    },
-    campaign_details: { name: 'Aldrune', id: 1},
-  }
-];
 
 const dummyMap: ExtendedMap = {
   getAbsoluteRouterUrl: () => "/map/url",
--- src/app/design/templates/marker/marker.stories.ts
+++ src/app/design/templates/marker/marker.stories.ts
@@ -2,39 +2,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryFn, moduleMetadata } from '@storybook/angular';
 import * as x from 'leaflet';
+import { dummyMarkers } from 'src/app/_services/article/marker.service.mock';
 import { MapMarker } from '../../../_models/mapMarker';
 import { OrganismsModule } from '../../organisms';
 import { MarkerComponent } from './marker.component';
 
 const y = x;
 
-const dummyMarker: MapMarker = {
-  pk: 1,
-  getAbsoluteRouterUrl: () => "https://example.com/map-marker/1",
-  color: "#ff0000",
-  icon: "plus",
-  latitude: 51.5074,
-  longitude: -0.1278,
-  map: 123,
-  map_details: { name: "Map of London" },
-  location: 456,
-  location_details: {
-    name: "London",
-    parent_location_name: "United Kingdom",
-    description: "Capital city of England",
-    sublocations: ["Westminster", "Camden", "Kensington and Chelsea"],
-  },
-  type: 789,
-  type_details: {
-    name: "Landmark",
-    is_text_marker: false,
-    icon: "fa-landmark",
-    color: "#ffa500",
-    pk: 789,
-    fontawesome_type: "solid",
-  },
-  campaign_details: { name: "Campaign of Adventures", id: 1 },
-};
+const dummyMarker: MapMarker = dummyMarkers[0];
 
 export default {
   title: 'DesignSystem/Templates/MarkerComponent',

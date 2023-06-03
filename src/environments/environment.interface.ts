d96081334d4d12298e36fd602549f0433b5791fc
Philipp Doerner <philippmdoerner@web.de>
2023-06-03 11:26:32 +0200

    narrow down Environment-typing via type union

--- src/environments/environment.interface.ts
+++ src/environments/environment.interface.ts
@@ -1,5 +1,7 @@
+export type EnvironmentKind = 'PRODUCTION' | 'DEVELOPMENT';
+
 export interface Environment{
-  kind: string;
+  kind: EnvironmentKind;
   backendDomain: string;
   apiUrl: string;
   defaultTitle: string; //The default title shown in the tab heading in the browser

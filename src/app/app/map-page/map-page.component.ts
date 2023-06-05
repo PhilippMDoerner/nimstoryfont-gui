c85dcb1e04766bebb7ba2173f84e04cd0fd65e73
Philipp Doerner <philippmdoerner@web.de>
2023-06-05 21:43:44 +0200

    Add map page

---
+++ src/app/app/map-page/map-page.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, first, map } from 'rxjs';
+import { ExtendedMap } from 'src/app/_models/map';
+import { OverviewItem } from 'src/app/_models/overview';
+import { TokenService } from 'src/app/_services/utils/token.service';
+import { environment } from 'src/environments/environment';
+import { deleteMap, loadMap } from '../app.actions';
+import { selectCurrentCampaignName, selectMap, selectMapOverviewItems } from '../app.reducer';
+
+@Component({
+  selector: 'app-map-page',
+  templateUrl: './map-page.component.html',
+  styleUrls: ['./map-page.component.scss']
+})
+export class MapPageComponent implements OnInit{
+  canCreate$!: Observable<boolean>;
+  canDelete$!: Observable<boolean>;
+  canUpdate$!: Observable<boolean>;
+  map$!: Observable<ExtendedMap | undefined>;
+  mapChoices$!: Observable<OverviewItem[]>;
+  serverUrl = environment.backendDomain;
+    
+  constructor(
+    private tokenService: TokenService,
+    private store: Store,
+  ){}
+  
+  ngOnInit(): void {
+    const hasMemberPermissions$: Observable<boolean> = this.store
+      .select(selectCurrentCampaignName)
+      .pipe(map(name => this.tokenService.isCampaignMember(name as string)));;
+    
+    this.canCreate$ = hasMemberPermissions$;
+    this.canDelete$ = hasMemberPermissions$;
+    this.canUpdate$ = hasMemberPermissions$;
+    this.map$ = this.store.select(selectMap);
+    this.mapChoices$ = this.store.select(selectMapOverviewItems);
+  }
+  
+  mapDelete(map: ExtendedMap): void{
+    this.store.dispatch(deleteMap({ map }));
+  }
+  
+  mapChange(map: OverviewItem): void{
+    const mapName = map.name;
+    
+    this.store.select(selectCurrentCampaignName)
+      .pipe(first())
+      .subscribe(campaignName => {
+        const payload = { 
+          mapName, 
+          campaignName: campaignName as string 
+        };
+        this.store.dispatch(loadMap(payload));
+    });
+  }
+}

c36f07591af40538a892e6b08cbbe93f4b924676
Philipp Doerner <philippmdoerner@web.de>
2024-03-23 18:52:09 +0100

    Fix name change
    requiredRole had been renamed to requiredMinimumRole

--- src/app/_services/utils/permission.service.ts
+++ src/app/_services/utils/permission.service.ts
@@ -5,104 +5,107 @@ import { RoutingService } from '../routing.service';
 import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class LoginGuardService implements CanActivate{
-
+export class LoginGuardService implements CanActivate {
   constructor(
     private routingService: RoutingService,
     private tokenService: TokenService
-  ) { }
+  ) {}
 
-  canActivate(): boolean{
+  canActivate(): boolean {
     const isLoggedIn: boolean = this.isUserLoggedIn();
-    if (!isLoggedIn){
+    if (!isLoggedIn) {
       this.routingService.routeToPath('login');
     }
 
     return isLoggedIn;
   }
 
-  isUserLoggedIn(): boolean{
+  isUserLoggedIn(): boolean {
     return this.tokenService.hasValidJWTToken();
   }
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AdminGuardService implements CanActivate{
+export class AdminGuardService implements CanActivate {
   //Administrative Permissions are special in that their values are booleans. You either are, or aren't an admin.
   constructor(
     public tokenService: TokenService,
-    public routingService: RoutingService,
-  ){}
-  canActivate(route: ActivatedRouteSnapshot): boolean{
+    public routingService: RoutingService
+  ) {}
+  canActivate(route: ActivatedRouteSnapshot): boolean {
     if (!this.isUserLoggedIn()) {
       this.routingService.routeToPath('login');
       return false;
     }
-    
+
     const hasRequiredPermissions: boolean = this.isGeneralAdmin();
-    if(!hasRequiredPermissions){
+    if (!hasRequiredPermissions) {
       this.routingService.routeToPath('campaign-overview');
     }
-    return hasRequiredPermissions
+    return hasRequiredPermissions;
   }
 
-  isUserLoggedIn(): boolean{
+  isUserLoggedIn(): boolean {
     return this.tokenService.hasValidJWTToken();
   }
 
-  isGeneralAdmin(): boolean{
+  isGeneralAdmin(): boolean {
     return this.tokenService.isSuperUser() || this.tokenService.isAdmin();
   }
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CampaignGuardService extends AdminGuardService{
+export class CampaignGuardService extends AdminGuardService {
   //Administrative Permissions are special in that their values are booleans. You either are, or aren't an admin.
-  constructor(
-    tokenService: TokenService,
-    routingService: RoutingService,
-  ){super(tokenService, routingService)}
+  constructor(tokenService: TokenService, routingService: RoutingService) {
+    super(tokenService, routingService);
+  }
 
-  override canActivate(route: ActivatedRouteSnapshot): boolean{
+  override canActivate(route: ActivatedRouteSnapshot): boolean {
     if (!this.isUserLoggedIn()) {
       this.routingService.routeToPath('login');
       return false;
     }
 
-    if(this.isGeneralAdmin()) return true;
+    if (this.isGeneralAdmin()) return true;
 
     const campaignNameOfRoute: string = route.params['campaign'];
-    if (campaignNameOfRoute == null) throw "Invalid Route Exception. The campaign-route you're trying to access has no campaign name parameter";
+    if (campaignNameOfRoute == null)
+      throw "Invalid Route Exception. The campaign-route you're trying to access has no campaign name parameter";
 
-    const hasNoRoleInCampaign: boolean = this.tokenService.getCampaignRole(campaignNameOfRoute) == null;
-    if (hasNoRoleInCampaign){
+    const hasNoRoleInCampaign: boolean =
+      this.tokenService.getCampaignRole(campaignNameOfRoute) == null;
+    if (hasNoRoleInCampaign) {
       this.routingService.routeToPath('campaign-overview');
       return false;
     }
 
-    const requiredMiniumRole: CampaignRole = route.data['requiredRole']; 
-    if (requiredMiniumRole == null) throw "Invalid Route Exception. The campaign-route you're trying to access has no defined minimum role needed to access it";
+    const requiredMiniumRole: CampaignRole = route.data['requiredMinimumRole'];
+    if (requiredMiniumRole == null)
+      throw "Invalid Route Exception. The campaign-route you're trying to access has no defined minimum role needed to access it";
 
     return this.hasRoleOrBetter(campaignNameOfRoute, requiredMiniumRole);
   }
 
-  hasRoleOrBetter(campaignName: string, role: CampaignRole): boolean{
-    const isCampaignAdmin: boolean = this.tokenService.isCampaignAdmin(campaignName);
-    const isCampaignMember: boolean = this.tokenService.isCampaignMember(campaignName);
-    const isCampaignGuest: boolean = this.tokenService.isCampaignGuest(campaignName);
+  hasRoleOrBetter(campaignName: string, role: CampaignRole): boolean {
+    const isCampaignAdmin: boolean =
+      this.tokenService.isCampaignAdmin(campaignName);
+    const isCampaignMember: boolean =
+      this.tokenService.isCampaignMember(campaignName);
+    const isCampaignGuest: boolean =
+      this.tokenService.isCampaignGuest(campaignName);
 
     if (role === 'admin') return isCampaignAdmin;
     if (role === 'member') return isCampaignAdmin || isCampaignMember;
-    if (role === 'guest') return isCampaignAdmin || isCampaignMember || isCampaignGuest;
-    
+    if (role === 'guest')
+      return isCampaignAdmin || isCampaignMember || isCampaignGuest;
+
     return false;
   }
 }

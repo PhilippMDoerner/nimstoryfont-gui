b060b0e3265bf33c508a7a170dbe09c874923032
PhilippMDoerner <philipp3000doerner@web.de>
2024-09-28 12:03:48 +0200

    Enable routing service to deal with child-routes

--- src/app/_services/routing.service.ts
+++ src/app/_services/routing.service.ts
@@ -2,81 +2,80 @@ import { Injectable } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RoutingService {
-  private NONE_STRING = "None";
-  
-  constructor(
-    private router: Router,
-  ) {}
+  private NONE_STRING = 'None';
 
-  public routeToPath(routeName: string, params: any = {}): void{
-    const routePath: string = this.getRoutePath(routeName, params)
-    const cleanedObjectUrl: string = this.replaceSpecialUnicodeCharacters(routePath);
+  constructor(private router: Router) {}
+
+  public routeToPath(routeName: string, params: any = {}): void {
+    const routePath: string = this.getRoutePath(routeName, params);
+    const cleanedObjectUrl: string =
+      this.replaceSpecialUnicodeCharacters(routePath);
     this.router.navigateByUrl(cleanedObjectUrl);
   }
 
-
-  public getRoutePath(routeName: string, params: any = {}): string{
+  public getRoutePath(routeName: string, params: any = {}): string {
     let variableRoutePath = this.getVariableRoutePathByName(routeName);
 
-    if (this.hasPathVariables(variableRoutePath)){
-      const variableNames: string[] = this.getPathVariableNames(variableRoutePath);
-      for (let variableName of variableNames){
-        if(!params.hasOwnProperty(variableName)){
+    if (this.hasPathVariables(variableRoutePath)) {
+      const variableNames: string[] =
+        this.getPathVariableNames(variableRoutePath);
+      for (let variableName of variableNames) {
+        if (!params.hasOwnProperty(variableName)) {
           throw `Tried to create path for route ${routeName} but lacked parameter ${variableName}`;
         }
-        
+
         const hasParamWithNullValue = params[variableName] === null;
-        if (params[variableName] === null){
+        if (params[variableName] === null) {
           params[variableName] = this.NONE_STRING;
         }
-        variableRoutePath = variableRoutePath.replace(`:${variableName}`, params[variableName]);
+        variableRoutePath = variableRoutePath.replace(
+          `:${variableName}`,
+          params[variableName],
+        );
       }
     }
 
     return `/${variableRoutePath}`;
   }
 
-
-  public routeToErrorPage(error: number| any): void{
-    if (typeof error !== "number" && !error.hasOwnProperty("status")){
+  public routeToErrorPage(error: number | any): void {
+    if (typeof error !== 'number' && !error.hasOwnProperty('status')) {
       // this.warning.showWarning(error);
-      throw "Incorrect error input. The input does not contain an error status or an object with the error status. Can not route to error page without error status.";
+      throw 'Incorrect error input. The input does not contain an error status or an object with the error status. Can not route to error page without error status.';
     }
-    
-    if (typeof error !== "number" && error.hasOwnProperty("status")) error = error.status;
-  
+
+    if (typeof error !== 'number' && error.hasOwnProperty('status'))
+      error = error.status;
+
     const errorStatusParam: string = `${error}`;
-    this.routeToPath("error", {errorStatus: errorStatusParam});
+    this.routeToPath('error', { errorStatus: errorStatusParam });
   }
 
-
-  public routeNameMatches(route: ActivatedRoute, routeName: string): boolean{
+  public routeNameMatches(route: ActivatedRoute, routeName: string): boolean {
     const routeData = route.snapshot.data;
     return routeData['name'] === routeName;
   }
-
 
   /**
    * Replaces special characters with their % Equivalent, as otherwise they cause problems in router.navigatebyUrl
    * @param {string} routePath - A route, such as /home
    * @returns {string} The same routePath as was given, but with special characters replaced
    */
-  private replaceSpecialUnicodeCharacters(routePath: string){
+  private replaceSpecialUnicodeCharacters(routePath: string) {
     return routePath
-      .replace("(", "%28")
-      .replace(")", "%29")
-      .replace("?", "\?")
-      .replace("†", "%E2%80%A0");
+      .replace('(', '%28')
+      .replace(')', '%29')
+      .replace('?', '?')
+      .replace('†', '%E2%80%A0');
   }
 
-  
-  private getVariableRouteByName(routeName: string): Route{
+  private getVariableRouteByName(routeName: string): Route {
     const routesWithRouteName = this.getRoutesWithName(routeName);
-    
-    switch(routesWithRouteName.length){
+
+    switch (routesWithRouteName.length) {
       case 1:
         return routesWithRouteName[0];
       case 0:
@@ -86,39 +85,53 @@ export class RoutingService {
     }
   }
 
-  
-  private getVariableRoutePathByName(routeName: string): string{
+  private getVariableRoutePathByName(routeName: string): string {
     const targetRouteObject: Route = this.getVariableRouteByName(routeName);
     return targetRouteObject.path as string;
   }
 
-  
-  private hasPathVariables(routePath: string): boolean{
+  private hasPathVariables(routePath: string): boolean {
     return routePath.includes('/:');
   }
 
-  
-  public hasRoutePath(routeName: string): boolean{
+  public hasRoutePath(routeName: string): boolean {
     const routesWithRouteName = this.getRoutesWithName(routeName);
     return routesWithRouteName.length > 0;
   }
-  
-  private getRoutesWithName(routeName: string): Route[]{
-    const routes: Route[] = this.router.config;
-    return routes.filter(pathObject => {
+
+  private getRoutesWithName(routeName: string): Route[] {
+    const routes: Route[] = this.getEndRoutes();
+    return routes.filter((pathObject) => {
       const routeData = pathObject.data;
-      if(routeData == null){
+      if (routeData == null) {
         return false;
       }
       return routeData['name'] === routeName;
     });
   }
 
-  
-  private getPathVariableNames(routePath: string): string[]{
-    const routeSegments: string[] = routePath.split("/");
-    const pathVariables: string[] = routeSegments.filter((segment: string) => segment.startsWith(":"));
+  private getEndRoutes(routes: Route[] = this.router.config): Route[] {
+    let endRoutes: Route[] = [];
+    routes.forEach((route) => {
+      const isEndRoute = route.children == null;
+      if (isEndRoute) {
+        endRoutes.push(route);
+      } else {
+        const childrenEndRoutes = this.getEndRoutes(route.children);
+        endRoutes = endRoutes.concat(childrenEndRoutes);
+      }
+    });
+    return endRoutes;
+  }
+
+  private getPathVariableNames(routePath: string): string[] {
+    const routeSegments: string[] = routePath.split('/');
+    const pathVariables: string[] = routeSegments.filter((segment: string) =>
+      segment.startsWith(':'),
+    );
     const variableNameStartIndex = 1;
-    return pathVariables.map(segment => segment.slice(variableNameStartIndex));
+    return pathVariables.map((segment) =>
+      segment.slice(variableNameStartIndex),
+    );
   }
 }

c584602a89a12a1f2b67d2ae5fa2876a5ed88d9a
PhilippMDoerner <philipp3000doerner@web.de>
2024-10-03 23:27:07 +0200

    Refactor site admin towards services

--- src/app/core/site-administration-page/site-administration-page.component.ts
+++ src/app/core/site-administration-page/site-administration-page.component.ts
@@ -1,39 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable, filter } from 'rxjs';
-import { PermissionGroup } from 'src/app/_models/auth';
-import { Campaign, WikiStatistics } from 'src/app/_models/campaign';
-import { User } from 'src/app/_models/user';
+import { Component, inject } from '@angular/core';
+import { GroupService } from 'src/app/_services/article/group.service';
+import { UserService } from 'src/app/_services/article/user.service';
+import { CampaignService } from 'src/app/_services/utils/campaign.service';
 import { environment } from 'src/environments/environment';
-import { selectAllSiteCampaigns, selectAllSiteGroups, selectAllSiteUsers, selectSiteStatistics } from '../app.reducer';
 
 @Component({
   selector: 'app-site-administration-page',
   templateUrl: './site-administration-page.component.html',
-  styleUrls: ['./site-administration-page.component.scss']
+  styleUrls: ['./site-administration-page.component.scss'],
 })
-export class SiteAdministrationPageComponent implements OnInit{
+export class SiteAdministrationPageComponent {
   serverUrl = environment.backendDomain;
-  allSiteUsers$!: Observable<User[]>;
-  allSiteCampaigns$!: Observable<Campaign[]>;
-  siteStatistics$!: Observable<WikiStatistics>;
-  allPermissionGroups$!: Observable<PermissionGroup[]>;
-  
-  constructor(
-    private store: Store
-  ){}
-  
-  ngOnInit(): void {
-    this.allSiteUsers$ = this.store.select(selectAllSiteUsers)
-      .pipe(filter(users => !!users)) as Observable<User[]>;
-      
-    this.allSiteCampaigns$ = this.store.select(selectAllSiteCampaigns)
-      .pipe(filter(users => !!users)) as Observable<Campaign[]>;
-    
-    this.siteStatistics$ = this.store.select(selectSiteStatistics)
-      .pipe(filter(users => !!users)) as Observable<WikiStatistics>;
-    
-    this.allPermissionGroups$ = this.store.select(selectAllSiteGroups)
-      .pipe(filter(users => !!users)) as Observable<PermissionGroup[]>;
-  }
+  readonly userService = inject(UserService);
+  readonly campaignService = inject(CampaignService);
+  readonly groupService = inject(GroupService);
+  allSiteUsers$ = this.userService.list.data;
+  allSiteCampaigns$ = this.campaignService.list.data;
+  siteStatistics$ = this.campaignService.statistics.data;
+  allPermissionGroups$ = this.groupService.list.data;
 }

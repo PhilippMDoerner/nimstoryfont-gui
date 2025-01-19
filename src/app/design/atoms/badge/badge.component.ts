d90a1fc74c906ae25d6e543f728f82b1ec26e486
PhilippMDoerner <philipp3000doerner@web.de>
2025-01-19 12:01:06 +0100

    Expand on badge functionality

--- src/app/design/atoms/badge/badge.component.ts
+++ src/app/design/atoms/badge/badge.component.ts
@@ -2,6 +2,7 @@ import { NgClass, NgTemplateOutlet } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   EventEmitter,
   input,
   Output,
@@ -11,17 +12,29 @@ import { Icon } from '../_models/icon';
 import { IconComponent } from '../icon/icon.component';
 
 @Component({
-    selector: 'app-badge',
-    templateUrl: './badge.component.html',
-    styleUrls: ['./badge.component.scss'],
-    imports: [NgTemplateOutlet, NgClass, IconComponent],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-badge',
+  templateUrl: './badge.component.html',
+  styleUrls: ['./badge.component.scss'],
+  imports: [NgTemplateOutlet, NgClass, IconComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BadgeComponent {
   kind = input.required<ElementKind>();
   text = input.required<string>();
   icon = input<Icon>();
   clickable = input(false);
+  maxLength = input<number | undefined>();
+
+  displayedText = computed(() => {
+    const maxLength = this.maxLength();
+    const shouldCutText = maxLength != null && this.text().length > maxLength;
+
+    if (shouldCutText) {
+      const cutText = this.text().slice(0, maxLength);
+      return `${cutText}...`;
+    }
+    return this.text();
+  });
 
   @Output() badgeClick = new EventEmitter<void>();
 }

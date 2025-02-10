a0b9e69ec5b1c2e6fe35990385357498b4cc71e4
PhilippMDoerner <philipp3000doerner@web.de>
2025-02-10 08:22:38 +0100

    Add autofocus to small-create-form

--- src/app/design/molecules/small-create-form/small-create-form.component.ts
+++ src/app/design/molecules/small-create-form/small-create-form.component.ts
@@ -2,16 +2,23 @@ import { NgTemplateOutlet } from '@angular/common';
 import {
   Component,
   computed,
+  DestroyRef,
+  ElementRef,
   EventEmitter,
+  inject,
   input,
   Output,
   signal,
+  viewChild,
 } from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { FormGroup } from '@angular/forms';
+import { take } from 'rxjs';
 import { HotkeyDirective } from 'src/app/_directives/hotkey.directive';
 import { ElementKind } from 'src/app/design/atoms/_models/button';
 import { BadgeComponent } from 'src/app/design/atoms/badge/badge.component';
 import { ButtonComponent } from 'src/app/design/atoms/button/button.component';
+import { filterNil } from 'src/utils/rxjs-operators';
 
 type State = 'DISPLAY' | 'CREATE';
 export type DisableableOption<T> = { value: T; disabled: boolean };
@@ -33,13 +40,21 @@ export class SmallCreateFormComponent<T> {
 
   @Output() create: EventEmitter<T> = new EventEmitter();
 
+  destroyRef = inject(DestroyRef);
   selectFieldName = computed(() => `select-' + ${String(this.labelProp())}`);
+  selectElement = viewChild<ElementRef<HTMLElement>>('select');
+  selectElement$ = toObservable(this.selectElement);
   form = new FormGroup({});
   userModel: Partial<T> = {};
   state = signal<State>('DISPLAY');
 
   changeState(newState: State) {
     this.state.set(newState);
+    if (newState === 'CREATE') {
+      this.selectElement$
+        .pipe(filterNil(), takeUntilDestroyed(this.destroyRef), take(1))
+        .subscribe((element) => element.nativeElement.focus());
+    }
   }
 
   onChange(event: any) {

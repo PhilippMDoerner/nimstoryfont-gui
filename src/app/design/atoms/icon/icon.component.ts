2e8ea98177d7a992101549454a15462164f828dd
PhilippMDoerner <philipp3000doerner@web.de>
2025-03-09 11:24:57 +0100

    Reduce unnecessary HTML tags from icon

--- src/app/design/atoms/icon/icon.component.ts
+++ src/app/design/atoms/icon/icon.component.ts
@@ -7,14 +7,22 @@ import {
 import { Icon, IconKind, toIconKind } from '../_models/icon';
 
 @Component({
-    selector: 'app-icon',
-    templateUrl: './icon.component.html',
-    styleUrls: ['./icon.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: []
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  host: {
+    '[class]': 'classes()',
+  },
 })
 export class IconComponent {
   icon = input.required<Icon>();
 
   iconType = computed<IconKind | undefined>(() => toIconKind(this.icon()));
+
+  classes = computed(() => {
+    if (!this.iconType()) return '';
+    return `${this.iconType()} fa-${this.icon()}`;
+  });
 }

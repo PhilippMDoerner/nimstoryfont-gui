13266297e647bb99c451077ee05ae1205d9d5768
PhilippMDoerner <philipp3000doerner@web.de>
2025-03-10 13:32:25 +0100

    Add label for select

--- src/app/design/molecules/choice-select/choice-select.component.ts
+++ src/app/design/molecules/choice-select/choice-select.component.ts
@@ -5,18 +5,20 @@ import {
   input,
   Output,
 } from '@angular/core';
+import { componentId } from 'src/utils/DOM';
 
 @Component({
-    selector: 'app-choice-select',
-    templateUrl: './choice-select.component.html',
-    styleUrls: ['./choice-select.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: []
+  selector: 'app-choice-select',
+  templateUrl: './choice-select.component.html',
+  styleUrls: ['./choice-select.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
 })
 export class ChoiceSelectComponent {
   choices = input.required<any[]>();
   labelProp = input.required<string>();
   selectedLabelValue = input.required<string | undefined>();
+  id = input<string>(componentId());
 
   @Output() choiceSelect: EventEmitter<any> = new EventEmitter();
 

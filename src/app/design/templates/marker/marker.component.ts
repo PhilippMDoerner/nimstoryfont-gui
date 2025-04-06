2d2667ab1f0021783212ad2506caf0266a1035ea
PhilippMDoerner <philipp3000doerner@web.de>
2025-04-06 20:57:34 +0200

    Migrate to OnPush

--- src/app/design/templates/marker/marker.component.ts
+++ src/app/design/templates/marker/marker.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnChanges, OnInit, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+} from '@angular/core';
 import { MapMarker } from 'src/app/_models/mapMarker';
 import { RoutingService } from 'src/app/_services/routing.service';
 import { PageContainerComponent } from '../../organisms/page-container/page-container.component';
@@ -19,48 +26,33 @@ import { ArticleFooterComponent } from '../../molecules/article-footer/article-f
     IconComponent,
     ArticleFooterComponent,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MarkerComponent implements OnInit, OnChanges {
-  @Input() marker!: MapMarker;
-  @Input() canDelete = false;
-  @Input() canUpdate = false;
+export class MarkerComponent {
+  marker = input.required<MapMarker>();
+  canDelete = input<boolean>(false);
+  canUpdate = input<boolean>(false);
 
   readonly markerDelete = output<MapMarker>();
 
-  locationUrl!: string;
-  updateUrl!: string;
+  private readonly routingService = inject(RoutingService);
 
-  constructor(private routingService: RoutingService) {}
-
-  ngOnInit(): void {
-    const parentName = this.marker.location_details?.parent_location_name;
-    const locationName = this.marker.location_details?.name;
-    const campaignName = this.marker.campaign_details.name;
-
-    this.locationUrl = this.routingService.getRoutePath('location', {
-      parent_name: parentName,
-      name: locationName,
-      campaign: campaignName,
+  locationUrl = computed(() => {
+    const { campaign_details, location_details } = this.marker();
+    return this.routingService.getRoutePath('location', {
+      parent_name: location_details?.parent_location_name,
+      name: location_details?.name,
+      campaign: campaign_details.name,
     });
+  });
+  updateUrl = computed(() => {
+    const { campaign_details, location_details, map_details } = this.marker();
 
-    this.setUrls();
-  }
-
-  ngOnChanges(): void {
-    this.setUrls();
-  }
-
-  private setUrls() {
-    const parentName = this.marker.location_details?.parent_location_name;
-    const locationName = this.marker.location_details?.name;
-    const campaignName = this.marker.campaign_details.name;
-    const mapName = this.marker.map_details?.name;
-
-    this.updateUrl = this.routingService.getRoutePath('marker-update', {
-      parent_location_name: parentName,
-      campaign: campaignName,
-      location_name: locationName,
-      map_name: mapName,
+    return this.routingService.getRoutePath('marker-update', {
+      parent_location_name: location_details?.parent_location_name,
+      campaign: campaign_details.name,
+      location_name: location_details?.name,
+      map_name: map_details?.name,
     });
-  }
+  });
 }
